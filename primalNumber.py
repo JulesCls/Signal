651276ffc7e3a0fb7125d3690d6f6d2172e8215c
Jules Colas <jules.colas08@gmail.com>
2022-09-28 14:49:24 +0200

    primal number algo

---
+++ primalNumber.py
@@ -0,0 +1,26 @@
+def isPrime(n):
+  if n == 1:
+    result = True
+  else:
+    for i in range(2,n):
+        if (n%i) == 0:
+            result = False
+            break
+    else:
+        result = True  
+    return result
+
+def calculateNFirstPrimeNumber(n):
+    primeNumbers = [2]
+    numberToTest = 3
+    while(len(primeNumbers) < n):
+        for primeNumber in primeNumbers:
+            if(numberToTest % primeNumber ) == 0:
+                break
+        else:
+            primeNumbers.append(numberToTest)
+        numberToTest+=1
+    primeNumbers.insert(0,1)
+    return(primeNumbers)
+
+print(calculateNFirstPrimeNumber(100))
