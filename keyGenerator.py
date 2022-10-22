8a3d00fb95c8d704426d9bf253fdc313f2cbfdf6
Jules Colas <jules.colas08@gmail.com>
2022-10-22 18:11:28 +0200

    start of xorshift generation number

--- keyGenerator.py
+++ keyGenerator.py
@@ -38,7 +38,3 @@ if __name__ == "__main__":
     privateKeys = generatePrivateKeys(public)
     privateKey = definePrivateKey(privateKeys)
     print(privateKey)
-
-           
-
-    
--- utils.py
+++ utils.py
@@ -1,13 +1,25 @@
+import secrets
+
 def addIterationToDictionary(number,dictionary):
     if(number in dictionary):
         dictionary[number] +=1
     else:
         dictionary[number] = 1
 
+def generateRandomSplitInt(numberSize, sectionNumber):
+    value = secrets.randbits(numberSize)
+    splitNumber = []
+    sectionSize = numberSize//sectionNumber
+    if numberSize % sectionNumber != 0:
+        return "Cannot split because of unequal sections"
+    else:
+        mask = (1 << sectionSize) - 1
+        for i in range(sectionNumber):
+            splitNumber.append(value & mask)
+            value >>= sectionSize
+        splitNumber.reverse()
+        return splitNumber
+
 if __name__ == "__main__":
-    dic = {2 : 3, 1:1}
-    addIterationToDictionary(2,dic)
-    addIterationToDictionary(2,dic)
-    addIterationToDictionary(4,dic)
-    print(dic)
+    print(generateSplitInt(128,4))
     
