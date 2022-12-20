d342e53d2fce2e05ed44faf82b8ef422ad2d28d8
Deussandouze <florianbourrier@gmail.com>
2022-12-20 13:44:17 +0100

    new sha256 implementation

--- HMAC256.py
+++ HMAC256.py
@@ -1,37 +1,69 @@
 from sha256 import Sha256
-import primalNumber,utils
+from newsha256 import SHA256
+import primalNumber,utils, struct
+from decimal import Decimal
 
 
 def HMAC_256(key:int,m:int):
     sha256 = Sha256()
     opad = 0x5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c
     ipad = 0x3636363636363636363636363636363636363636363636363636363636363636
-    print(len(bin(ipad)))
 
     if len(bin(key)[2:]) > 256:
         key = sha256.int_hash(key)
-    while(len(bin(key)[2:0]) > 256):
-        key <<= 1
+    else:
+        key = key << (256 - len(bin(key)[2:]) - 1)
 
-    left_operation = key ^ opad
-    right_operation = sha256.int_hash((key ^ ipad) | m)
+    k1 = ipad^key
+    k2 = opad^key
 
-    return sha256.int_hash(left_operation | right_operation)
+
+    h1 =  sha256.int_hash(str(k1) + str(m))
+    hmac = sha256.int_hash(str(k2) + str(h1))
+
+    return hmac
+
+
+
+import hashlib
+
+def compute_hmac(key, message):
+    # Si la clé est plus courte que la longueur du bloc de l'algorithme de hachage (64 octets pour SHA256), elle doit être étendue
+    if len(key) < 32:
+        key += b'\x00' * (32 - len(key))
+    elif len(key) > 32:
+        key = hashlib.sha256(key).digest()
+
+    
+    # Créer deux pads de clé, K1 et K2
+    k1 = bytearray(key)
+    k2 = bytearray(key)
+    
+    for i in range(32):
+        k1[i] ^= 0x36
+        k2[i] ^= 0x5c
+    
+    
+    h1 = hashlib.sha256(k1 + message).digest()
+    hmac_hash = hashlib.sha256(k2 + h1).hexdigest()
+
+    # Retournez le hachage sous forme de chaîne hexadécimale
+    return hmac_hash
 
 if __name__ == "__main__":
     sha256 = Sha256()
-    key = 1347970089728574169630159058194593715446431332080111800307126033557879564623561271064755333325920715118756313806805813307389675662594418400047194434929731
-    message = "hello je suis un message"
-    print(message)
+    key = utils.mergeBinaryString('my key')
+    message = "my message"
     message = utils.mergeBinaryString(message)
-    print(message)
-    print(utils.unMergeBinaryString(message))
+    print(hex(HMAC_256(key,message)))
     print(hex(HMAC_256(key,message)))
 
 
-    key = utils.unMergeBinaryString(key).encode()
-    message = 'my message'.encode()
-    import hashlib
-    import hmac
-    hmac_obj = hmac.new(key, message,hashlib.sha256)
-    print(hmac_obj.hexdigest())
+    key = utils.unMergeBinaryString(key).encode('utf-16-be')
+    message = 'my message'.encode('utf-16-be')
+    print(compute_hmac(key,message))
+    
+    # import hashlib
+    # import hmac
+    # hmac_obj = hmac.new(key, message,hashlib.sha256)
+    # print(hmac_obj.hexdigest())
--- sha256.py
+++ sha256.py
@@ -1,156 +1,187 @@
-import utils, lsfr, hashlib
+import struct
 
-def Ch(e,f,g):
-    return (e&f)^(~(e)&g)
+class SHA256:
+    def __init__(self):
+        # Initialise les variables nécessaires
+        self.h0 = 0x6a09e667
+        self.h1 = 0xbb67ae85
+        self.h2 = 0x3c6ef372
+        self.h3 = 0xa54ff53a
+        self.h4 = 0x510e527f
+        self.h5 = 0x9b05688c
+        self.h6 = 0x1f83d9ab
+        self.h7 = 0x5be0cd19
 
-def Maj(a,b,c):
-    return (a&b)^(a&c)^(b&c)
+        self.k = [
+            0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b,
+            0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
+            0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7,
+            0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+            0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152,
+            0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+            0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc,
+            0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+            0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819,
+            0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08,
+            0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f,
+            0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+            0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+        ]
 
+    def _padding(self, message_bytes):
+        """
+        Ajoute des octets de padding au message binaire
+        """
+        ml = len(message_bytes) * 8  # Message length in bits
+        # Ajoute un '1' à la fin
+        message_bytes += b'\x80'
 
-def rotateValueXTimes(value,x):
-    for _ in range(x):
-        newBit = value & 1
-        if(newBit == 1 ):
-            value+= pow(2,32)
-        value >>= 1
-    return value
+        # Ajoute des zéros jusqu'à ce que le message soit de 448 bits (message_bytes.length*8 + 512 bits)
+        while len(message_bytes) % 64 != 56:
+            message_bytes += b'\x00'
 
+        # Ajoute la taille du message en bits (64 bits)
+        message_bytes += struct.pack(b'>Q', ml)
 
-def E0(x): #Used to do summ0 operation in w calculations
-    return rotateValueXTimes(x,2) ^ rotateValueXTimes(x,13) ^ rotateValueXTimes(x,22) 
+        return message_bytes
 
-def E1(x): #Used to do summ1 operation in w calculations
-    return rotateValueXTimes(x,6) ^ rotateValueXTimes(x,11) ^ rotateValueXTimes(x,25) 
+    def _split_chunks(self, padded_message):
+        """
+        Divise le message en chunks de 512 bits
+        """
+        # Découpe le message en chunks de 64 octets (64*8=512)
+        chunks = [padded_message[i:i+64] for i in range(0, len(padded_message), 64)]
+        return chunks
 
-def sigma0(x): #Used to do sigma0 operation in w calculations
-    return rotateValueXTimes(x,18) ^ rotateValueXTimes(x,7) ^ (x>>3) 
+    def _get_words(self, chunk):
+        """
+        Convertit un chunk de 64 octets en 16 mots de 32 bits
+        """
+        # Découpe le chunk en 16 mots de 32 bits
+        words = struct.unpack(b'>16L', chunk)
+        return list(words)
 
-def sigma1(x): #Used to do sigma1 operation in w calculations
-    return rotateValueXTimes(x,19) ^ rotateValueXTimes(x,17) ^ (x>>10) 
-class Sha256():
-    mod = pow(2,32)
-    textInput = ""
-    w = []
-    originalLength = 0
-    
-    ROUND_CONSTANTS = (
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-    )
-    N = 1
-    a = b = c = d = e = f = g = h = H0 = H1 = H2 = H2 = H4 = H5 = H6 = H7 = None
+    def _extend_words(self, words):
+        """
+        Étend la liste des mots de 16 à 64
+        """
+        for i in range(16, 64):
+            s0 = self._right_rotate(words[i-15], 7) ^ \
+                self._right_rotate(words[i-15], 18) ^ (words[i-15] >> 3)
+            s1 = self._right_rotate(words[i-2], 17) ^ \
+                self._right_rotate(words[i-2], 19) ^ (words[i-2] >> 10)
+            words.append((words[i-16] + s0 + words[i-7] + s1) & 0xffffffff)
+        return words
 
-    def initializeH(self):
-        self.H0 = 0x6a09e667
-        self.H1 = 0xbb67ae85
-        self.H2 = 0x3c6ef372
-        self.H3 = 0xa54ff53a
-        self.H4 = 0x510e527f
-        self.H5 = 0x9b05688c
-        self.H6 = 0x1f83d9ab
-        self.H7 = 0x5be0cd19
-    
-    def hash(self,textInput) -> str:
-        self.textInput = str(textInput)
-        self.preProcess() #prepare our block
-        self.initializeH()
-        for _ in range(self.N):
-            self.iterateThroughBlocks() #prepare w0 to 15
-            self.calculateW()  #calculate w16 to 63
-            self.setVariables() #set a,b,c.. to H0,H1,h2...
-            self.calculateVariables() #calculate a,b,c... transformations for the block
-            self.setHVariables() #assign a+H0 to h0... 
-            self.N-=1
-        self.postProcess() # concatenate hex values
+    def _right_rotate(self, n, d):
+        """
+        Effectue un rotation de n à droite de d bits
+        """
+        return (n >> d) | (n << (32 - d)) & 0xffffffff
+
+    def _compress(self, words):
+        """
+        Compresse 512 bits en 256
+        """
+        a, b, c, d, e, f, g, h = self.h0, self.h1, self.h2, self.h3, self.h4, self.h5, self.h6, self.h7
+
+        for i in range(64):
+            s1 = self._right_rotate(e, 6) ^ self._right_rotate(e, 11) ^ \
+                 self._right_rotate(e, 25)
+            ch = (e & f) ^ ((~e) & g)
+            temp1 = (h + s1 + ch + self.k[i] + words[i]) & 0xffffffff
+            s0 = self._right_rotate(a, 2) ^ self._right_rotate(a, 13) ^ \
+                 self._right_rotate(a, 22)
+            maj = (a & b) ^ (a & c) ^ (b & c)
+            temp2 = (s0 + maj) & 0xffffffff
+
+            h = g
+            g = f
+            f = e
+            e = (d + temp1) & 0xffffffff
+            d = c
+            c = b
+            b = a
+            a = (temp1 + temp2) & 0xffffffff
+
+        self.h0 = (self.h0 + a) & 0xffffffff
+        self.h1 = (self.h1 + b) & 0xffffffff
+        self.h2 = (self.h2 + c) & 0xffffffff
+        self.h3 = (self.h3 + d) & 0xffffffff
+        self.h4 = (self.h4 + e) & 0xffffffff
+        self.h5 = (self.h5 + f) & 0xffffffff
+        self.h6 = (self.h6 + g) & 0xffffffff
+        self.h7 = (self.h7 + h) & 0xffffffff
+
+    def _hexdigest(self):
+        """
+        Retourne l'empreinte hexadécimale
+        """
+        return '{:08x}{:08x}{:08x}{:08x}{:08x}{:08x}{:08x}{:08x}'.format(self.h0, self.h1, self.h2, self.h3,self.h4, self.h5, self.h6, self.h7)
+
+    def digest(self, message_bytes):
         
-        return self.textInput
+        return bytes.fromhex(self.hexdigest(message_bytes))
+    
+    def update(self, data):
+        data = self.padding(data)
+        blocks = self.to_blocks(data)
+        for block in blocks:
+            self.process_block(block)
 
-    def int_hash(self,textInput) -> int:
-        return int(self.hash(str(textInput)),16)
+    
+    def hexdigest(self, message_bytes):
+        self.h0 = 0x6a09e667
+        self.h1 = 0xbb67ae85
+        self.h2 = 0x3c6ef372
+        self.h3 = 0xa54ff53a
+        self.h4 = 0x510e527f
+        self.h5 = 0x9b05688c
+        self.h6 = 0x1f83d9ab
+        self.h7 = 0x5be0cd19
+        """
+        Retourne l'empreinte hexadecimale SHA256 du message binaire passé en paramètre
+        """
+        # Ajoute des octets de padding au message binaire
+        padded_message = self._padding(message_bytes)
 
-    def preProcess(self): #preprocess string, add 1 and x 0 to the end, to make it a multiple of 512
-        bytesArray = utils.stringToByteArray(self.textInput)
-        self.originalLength = len(bytesArray)*8
-        self.N = (self.originalLength // 512)+1
-        if (self.originalLength > (self.N*512)-64):
-            self.N+=1
-        self.textInput = utils.bytesArrayToInt(bytesArray)
-        self.textInput <<= 1
-        self.textInput+=1
-        self.textInput <<= (self.N*512-(self.originalLength+1))
-        self.textInput+=self.originalLength
+        # Découpe le message en chunks de 512 bits
+        chunks = self._split_chunks(padded_message)
 
-    def postProcess(self): #hash to hex
-        self.textInput = "" + hex(self.H0)[2:]+hex(self.H1)[2:]+hex(self.H2)[2:]+hex(self.H3)[2:]+hex(self.H4)[2:]+hex(self.H5)[2:]+hex(self.H6)[2:]+hex(self.H7)[2:]
+        # Traitement des chunks
+        for chunk in chunks:
+            # Convertit le chunk en mots de 32 bits
+            words = self._get_words(chunk)
 
-    def iterateThroughBlocks(self): #prepare first w values
-        self.w = []
-        input = self.textInput >> ((self.N-1)*512)
-        input = (int("1"*512,2)) & input
-        for _ in range(16):
-            self.w.insert(0,input & (int("1"*32,2)))
-            input = input>>32
+            # Étend la liste des mots à 64
+            words = self._extend_words(words)
 
-    def calculateW(self): # calculate following w values
-        for t in range(16,64):
-            wt = (sigma1(self.w[t-2]) + self.w[t-7] + sigma0(self.w[t-15]) + self.w[t-16]) % self.mod
-            wt%=pow(2,32)
-            self.w.append(wt)
+            # Compresse 512 bits en 256
+            self._compress(words)
 
-    def calculateVariables(self): #calculate a,b,c... transformations for the block
-        for t in range(64):                
-            T1 = (self.h + E1(self.e) + Ch(self.e,self.f,self.g) + self.ROUND_CONSTANTS[t] + self.w[t]) % self.mod
-            T2 = (E0(self.a) + Maj(self.a,self.b,self.c)) % self.mod
-            self.h = self.g
-            self.g = self.f
-            self.f = self.e
-            self.e = (self.d + T1) % self.mod
-            self.d = self.c
-            self.c = self.b
-            self.b = self.a
-            self.a = (T1 + T2) % self.mod
-            
-    def setVariables(self):   #set a,b,c.. to H0,H1,h2...
-        self.a = self.H0
-        self.b = self.H1
-        self.c = self.H2
-        self.d = self.H3
-        self.e = self.H4
-        self.f = self.H5
-        self.g = self.H6
-        self.h = self.H7
+        # Retourne le bytearray de l'empreinte hexadécimale
+        return self._hexdigest()
 
-    def setHVariables(self): #assign a+H0 to h0... 
-        self.H0 = (self.a + self.H0)%self.mod
-        self.H1 = (self.b + self.H1)%self.mod
-        self.H2 = (self.c + self.H2)%self.mod
-        self.H3 = (self.d + self.H3)%self.mod
-        self.H4 = (self.e + self.H4)%self.mod
-        self.H5 = (self.f + self.H5)%self.mod
-        self.H6 = (self.g + self.H6)%self.mod
-        self.H7 = (self.h + self.H7)%self.mod
-
-
+import hashlib
 
 if __name__ == "__main__":
-    sha = Sha256()
-    test = "sdjfhsdkfhj&é-èçà__çè_ç-è-(yiugshjkfnslkef usfhj kdsfhl ezuify è_-_èf (-sdçfè a_zfy ze))"
     
-    m = hashlib.sha256()
-    t2 = test
-    t2 = t2.encode('utf-16-be')
-    m.update(t2)
+    test = "sdjfhsdkfhj&é-èçà__çè_ç-è-(yiugshjkfnslkef usfhj kdsfhl ezuify è_-_èf (-sdçfè a_zfy ze))"
+    test = test.encode()
 
-    print(sha.int_hash(test))
-    print(sha.hash(test) == hex(sha.int_hash(test))[2:])
-    print(m.hexdigest())
-    print(sha.hash(test) == m.hexdigest())
+    sha = SHA256()
+    m = hashlib.sha256()
+    m.update(test)
+
+
+
+    res1 = sha.digest(test)
+    res2 = sha.hexdigest(test)
+    res3 = sha.hexdigest(test)
+    print(res1)
+    print(res2)
+    print(res3)
+    print(res1 == m.digest())
 
     
     
