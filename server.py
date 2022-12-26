def8b36ddad18e2ea70b41864395234ede00996e
Deussandouze <florianbourrier@gmail.com>
2022-12-26 23:38:30 +0100

    x3dh implementation start

--- server.py
+++ server.py
@@ -66,72 +66,18 @@ class Server:
 
     def update_user_info(self,user):
         data = self.check_public_info_of_user(user.get_name())
-        if user.share_info_to_server() != data:
+        if not data:
             self.publish_user_info(user)
         
 
     def connect_user(self,user):
         self.update_user_info(user)
 
-   
-
-    ##to rework
-
-
-    def _conversation_exists(self,conversation_directory) -> bool:
-        return os.path.exists(conversation_directory)
-           
-
-    def _check_for_pre_keys(self,conversation_directory,target) -> None|int:
-        file = os.path.join(conversation_directory,target+".keys")
-        target_info = None
-        if os.path.exists(file):
-            with open(file,"r") as f:
-                target_info = json.loads(f.read())
-                key_number = random.randint(0,len(target_info["OtPK"])-1)
-                target_info["OtPK"] = target_info["OtPK"][key_number] 
-                with open(os.path.join(conversation_directory,target+".key"),"w") as f2:
-                    f2.write(str(key_number))
-            os.remove(file)
-            return target_info            
-        return target_info
-
-    def _check_for_selected_pre_key(self,conversation_directory,target) -> None|int:
-        file = os.path.join(conversation_directory,target+".key")
-        if os.path.exists(file):
-            with(file,"r") as f:
-                key = int(f.read())
-                return key
-        return None
-
-
-    def connect_user_to_target(self,user,target):
-        conversation_directory = self.get_conversation_directory_name(user,target)
-        if not(self._conversation_exists(conversation_directory)):
-            os.makedirs(os.path.join(conversation_directory))
-            self.initialize_key_share(user,conversation_directory)
-        else:
-            key = self._check_for_pre_keys(conversation_directory,target)
-            if key is not None:
-                return key
-            else:
-                key = self._check_for_selected_pre_key(conversation_directory,target)
-                if key is not None:
-                    return key
-                    
-
-            
-
-
 
     def send_message(self,user,message:str,target:str) -> False:
         pass
             
         #else use ratchet
-
-    def initialize_conversation(self,user,target:str,conversation_directory:str):
-        self.initialize_key_share(user,conversation_directory)
-
     def get_conversation_directory_name(self,user,target:str) -> str:
         conversation_directory = self._server_directory_path
         username = user.get_name()
@@ -140,11 +86,6 @@ class Server:
         else:
             conversation_directory =  os.path.join(conversation_directory, username +"-"+ target)
         return conversation_directory
-
-    def initialize_key_share(self,user,conversation_directory:str) -> None:
-        with open(os.path.join(conversation_directory,user.get_name() + ".keys") ,"w") as f:
-            f.write(json.dumps(user.get_initialization_keys_x3dh()))
-
             
 
 
--- user.py
+++ user.py
@@ -16,14 +16,14 @@ class User:
     _id:idCouple #public/private id
     _server:Server = Server()
     _master_key = None
-    _pre_keys_list:List[int]
+    _otpk_list:List[int]
     _pk:idCouple
     _user_directory:str = None
     _current_target:str = None
 
 
     def __init__(self,_name:str) -> None:
-        self._pre_keys_list = list()
+        self._otpk_list = list()
         self._name = _name
         self._user_directory = os.path.abspath(os.path.join(os.path.dirname(__file__),self._name))
         self._id = {"public": None, "private" : None}
@@ -34,7 +34,7 @@ class User:
             self._pk["private"] = secrets.randbelow(self._server.get_public_prime()-2)
             self._pk["public"] = expo_rapide(self._server.get_public_generator(),self._pk["private"],self._server.get_public_prime())
             self.write_info()
-        self._pre_keys_list = self._load_pre_keys()
+        self._otpk_list = self._load_pre_otpk()
 
 
     def write_info(self): #write public/private couple into user_name/.priv file to load it later
@@ -76,10 +76,10 @@ class User:
     def share_info_to_server(self):
         elgamal = Elgamal()
         return {
-            "ID": self._id["public"],
-            "SigPK": self._pk["public"],
-            "Signature" : elgamal.sign(self._pk["public"].to_bytes(256,"big"),self._id["private"]),
-            "OtPK": [public for _,public in self.generate_pre_keys()]
+            "id": self._id["public"],
+            "pk": self._pk["public"],
+            "signature" : elgamal.sign(self._pk["public"].to_bytes(256,"big"),self._id["private"]),
+            "otpk": [public for _,public in self._otpk_list]
         }
     
     def send_message(self,message:str,target:str) -> bool:
@@ -92,6 +92,28 @@ class User:
         self._pk_secret = expo_rapide(self._server.get_public_generator(),self._pk_pub,self._server.get_public_prime())
         return [self._id["public"],self._pk_pub,elgamal.sign(self._pk_pub.to_bytes(256,"big"),self._id["private"])]
 
+
+    def generate_otpk(self):
+        new_pre_keys = []
+        for _ in range(4):
+            private_pre_key = secrets.randbelow(self._server.get_public_prime()-2)
+            new_pre_keys.append((private_pre_key,expo_rapide(self._server.get_public_generator(),private_pre_key,self._server.get_public_prime())))
+        self._otpk_list += new_pre_keys
+        self._save_otpk()
+        return new_pre_keys
+
+    def _load_pre_otpk(self):
+        directory = os.path.join(self._user_directory,"./otpk")
+        if os.path.exists(directory):
+            with open(directory,"r") as f:
+                return json.loads(f.read())
+        return self.generate_otpk()
+
+
+    def _save_otpk(self):
+        directory = os.path.join(self._user_directory,"./otpk")
+        with open(directory,"w") as f:
+            f.write(json.dumps(self._otpk_list))
     #connect to server
         #ask server if it has data 
         #send if not
@@ -102,32 +124,7 @@ class User:
         #si non a nous de récup les data si elles existentes sinon en attente
 
 
-    def _load_pre_keys(self):
-        directory = os.path.join(self._user_directory,"./otpk")
-        if os.path.exists(directory):
-            with open(directory,"r") as f:
-                return json.loads(f.read())
-        return []
-
-
-    def _save_pre_keys(self):
-        directory = os.path.join(self._user_directory,"./otpk")
-        with open(directory,"w") as f:
-            f.write(json.dumps(self._pre_keys_list))
-
-    def connect_to_conversation(self,target:str):
-        self._current_target = target
-        print(self._server.connect_user_to_target(self,target))
-
-
-    def generate_pre_keys(self):
-        new_pre_keys = []
-        for _ in range(4):
-            private_pre_key = secrets.randbelow(self._server.get_public_prime()-2)
-            new_pre_keys.append((private_pre_key,expo_rapide(self._server.get_public_generator(),private_pre_key,self._server.get_public_prime())))
-        self._pre_keys_list += new_pre_keys
-        self._save_pre_keys()
-        return new_pre_keys
+    
 
 if __name__ == "__main__":
     alice = User("Alice")
