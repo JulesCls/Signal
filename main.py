95931bea7efc4ebe2f0af9c737a420756630f964
Jules Colas <jules.colas08@gmail.com>
2022-12-29 15:02:42 +0100

    thread

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 
 from user import User
-from server import Server
+from multiprocessing import Process
 
-def show_channel_possibilites():
+def show_channel_possibilites(user:User,target_name:str):
     print("="*50)
-    print("Que souhaitez-vous faire ? ")
-    print("  1  -- Envoyer un message")
-    print("  2  -- Envoyer un fichier")
-    print("  3  -- Lire vos messages")
+    print(f"{user.get_name()} que souhaitez-vous faire ? ")
+    print(f"  1  -- Envoyer un message à {target_name}")
+    print(f"  2  -- Envoyer un fichier à {target_name}")
     print("quit -- quitter")
+    print("="*50)
+    print("\n"*1)
 
 def execute_input(input_str:str,user:User,target:str):
     if input_str == "1":
@@ -26,17 +27,17 @@ def execute_input(input_str:str,user:User,target:str):
         except Exception as e:
             print("*"*20 + "Erreur" + "*"*20)
             print(f"Impossible d'envoyer le message : {e}")
-    elif input_str == "3":
-        user.print_target_conversation(target)
 
 
 def channel_connection(user:User,target_name:str):
     user.connect_to_target(target_name)
     user_input = ""
-    user.print_target_conversation(target_name)
     while user_input != "quit":
-        show_channel_possibilites()
-        user_input = input("Choix :")
+        p = Process(target=user.print_target_conversation, args=(target_name,))
+        p.daemon = True
+        p = p.start()
+        show_channel_possibilites(user,target_name)
+        user_input = input()
         if user_input != "quit":
             execute_input(user_input,user,target_name)
     
@@ -46,9 +47,9 @@ def channel_connection(user:User,target_name:str):
 import os
 
 if __name__ == "__main__":
-    user_input = input("Entrez votre nom d'utilisateur:")
+    user_input = input("Entrez votre nom d'utilisateur: ")
     user = User(user_input)
-    print(f"Bonjour {user.get_name()}:")
+    print(f"Bonjour {user.get_name()} ! ")
     print("Il est possible de quitter l'application en utilisant la commande 'quit'")
     while user_input != "quit":
         user_input = input("A quel utilisateur souhaitez vous envoyer un message : ")
